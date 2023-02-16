5f907b14ff2ddc4b02018a434c741bf03e557d64
MILKO28 <beyumilki@gmail.com>
2023-02-16 12:21:14 -0800

    c programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*main entry point
+ *retrun: always 0(sucess)
+ */
+int main(void)
+{
+	printf("Size of a char: %zubyte(s)\n",sizeof(char));
+	printf("Size of an int: %zubyte(s)\n",sizeof(int));
+	printf("Size of a long int: %zubyte(s)\n",sizeof(long int));
+	printf("Size of a long long int: %zubyte(s)\n",sizeof(long long int));
+	printf("Size of a float: %zubyte(s)\n",sizeof(float));
+	return(0);
+}
