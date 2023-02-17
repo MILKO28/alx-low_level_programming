72a73f58737606dcc72de80feeb70a57b90080ca
MILKO28 <beyumilki@gmail.com>
2023-02-17 11:32:28 -0800

    printing 0-9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+int x=0;
+
+while( x < 10)
+{
+putchar(48 + x);
+x++;
+}
+printf("\n");
+return (0);
+}
