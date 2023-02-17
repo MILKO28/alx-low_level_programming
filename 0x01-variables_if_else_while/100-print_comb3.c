d21c0068537a4aae029442ac6bacd8957cbd8bbe
MILKO28 <beyumilki@gmail.com>
2023-02-17 12:22:12 -0800

    c programming

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+int y,z;
+for (y = '0';y < '9'; y++)
+{
+for (z = y + 1; z <= '9'; z++)
+{
+if (y != z)
+{
+
+putchar(y);
+putchar(z);
+if (y == '8' && z == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
+
