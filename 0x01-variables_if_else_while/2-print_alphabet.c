d70ddf070a3ce8b2cd180015ca7fde57db4785b1
MILKO28 <beyumilki@gmail.com>
2023-02-17 09:57:31 -0800

    c programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+    int alpha = 'a';
+    while (alpha <= 'z')
+{
+putchar (alpha);
+alpha++;
+}
+putchar ('\n');
+return (0);
+}
