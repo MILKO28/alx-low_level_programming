06c3e15f2115960e260d865ecd1e7a9feeb2060b
MILKO28 <beyumilki@gmail.com>
2023-02-22 14:10:53 -0800

    fun and nestedloops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,18 +9,17 @@
 
 void print_alphabet_x10(void)
 {
-        int i;
-	i = 0;
-        while  (i < 10)
-	{
-	int alpha = 'a';
-        while (alpha <= 'z')
-        {
-                putchar (alpha);
-                alpha++;
-        }
-
-        putchar ('\n');
-	i++;
-	}
+int i;
+i = 0;
+while  (i < 10)
+{
+int alpha = 'a';
+while (alpha <= 'z')
+{
+putchar (alpha);
+alpha++;
+}
+putchar ('\n');
+i++;
+}
 }
