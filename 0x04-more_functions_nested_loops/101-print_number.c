6f10d461e7327791b499ce622d7caa8852859877
MILKO28 <beyumilki@gmail.com>
2023-02-24 12:36:58 -0800

    functions and nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,56 +6,41 @@
  */
 void print_number(int n)
 {
-        if (n < 10 && n > 0)
-        {
-                putchar(n);
-        }
-        else if (n == 0)
-        {
-            putchar('0');
-        }
-        else if (n >= 10 && n < 100)
-        {
-
-            putchar(n/10 + '0');
-            putchar(n%10 + '0');
-
-        }
-        else if (n >= 100 && n < 1000)
-        {
-                putchar(n / 100 + '0');
-                n= n%100;
-
-        putchar(n / 10 + '0');
-        putchar(n + '0');
-        }
-        else if (n >=100 && n < 10000)
-        {
-            putchar(n / 1000 + '0');
-                n= n%1000;
-
-        putchar(n / 100 + '0');
-        n= n%100;
-         putchar(n / 10 + '0');
-         n=n%10;
-        putchar(n + '0');
-        }
-        else if (n <= -10 && n > -100)
-            {
-            putchar('-');
-            putchar( - n / 10 + '0');
-            n=n%10;
-            putchar(-n + '0');
-            }
-	else if (n >= 10 && n < 100)
-	{
-		_putchar(n % 10 + '0');
-         	_putchar(n + '0');
-	}
-	else if (n >= 100 && n < 1000)
-	{
-		_putchar(n % 100 + '0');
-		_putchar(n % 10 + '0');
-		_putchar(n + '0');
-	}
+if (n < 10 && n > 0)
+{
+putchar(n);
+}
+else if (n == 0)
+{
+putchar('0');
+}
+else if (n >= 10 && n < 100)
+{
+putchar(n / 10 + '0');
+putchar(n % 10 + '0');
+}
+else if (n >= 100 && n < 1000)
+{
+putchar(n / 100 + '0');
+n = n%100;
+putchar(n / 10 + '0');
+putchar(n + '0');
+}
+else if (n >=100 && n < 10000)
+{
+putchar(n / 1000 + '0');
+n = n % 1000;
+putchar(n / 100 + '0');
+n = n % 100;
+putchar(n / 10 + '0');
+n = n % 10;
+putchar(n + '0');
+}
+else if (n <= - 10 && n > - 100)
+{
+putchar('-');
+putchar( - n / 10 + '0');
+n = n % 10;
+putchar( - n + '0');
+}
 }
