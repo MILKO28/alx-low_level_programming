b04a068885990ad3deaeedc3904e3ee97a70deee
MILKO28 <beyumilki@gmail.com>
2023-02-27 11:44:36 -0800

    pointer and arrays

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: string to be revers
+ */
+
+void print_rev(char *s)
+{
+        int i, j = 0, len;
+
+        for (i = 0; s[i] != '\0'; i++)
+        {
+                j++;
+        }
+        len = j - 1;
+        for (; len >= 0; len--)
+        {
+                s[len];
+        }
+}
