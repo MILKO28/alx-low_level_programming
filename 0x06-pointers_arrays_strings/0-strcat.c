3cca2c8c44262d166a399b45a1c59fda786c3d15
MILKO28 <beyumilki@gmail.com>
2023-03-02 01:01:45 -0800

    More poiner and Arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,19 +3,21 @@
  * *_strncat - a function that concatenates two strings
  * @dest: input
  * @src: input
+ * Return: destination
  */
 
 char *_strcat(char *dest, char *src)
 {
-    int sum_1 = 0, i;
-    for (i = 0; dest[i] != '\0'; i++)
-    {
-        sum_1++;
-    }
-    for (i = 0; src[i] != '\0'; i++, sum_1++)
-    {
-       dest[sum_1] = src[i];
-    }
-   dest[sum_1] = '\0';
-   return dest;
+	int sum_1 = 0, i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		sum_1++;
+	}
+	for (i = 0; src[i] != '\0'; i++, sum_1++)
+	{
+		dest[sum_1] = src[i];
+	}
+	dest[sum_1] = '\0';
+	return (dest);
 }
