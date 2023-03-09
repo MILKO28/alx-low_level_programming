fbab189bd96e4df3763e74e36b8dccc028baf0dc
MILKO28 <beyumilki@gmail.com>
2023-03-09 11:51:18 -0800

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-int sqrt(int n, int i);
+int _sqrt(int n, int i);
+
 /**
  * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
  * @n: input
@@ -12,21 +13,21 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (sqrt(n, (n + 1) / 2));
+		return (_sqrt(n, (n + 1) / 2));
 }
 
 /**
- * sqrt - checks if perfect square
+ * _sqrt - checks if perfect square
  * @n: input
  * @i: counter
  * Return: if square root
  */
-int sqrt(int n, int i)
+int _sqrt(int n, int i)
 {
 	if (i < 1)
 		return (-1);
 	else if (i * i == n)
 		return (i);
 	else
-		return (sqrt(n, i - 1));
+		return (_sqrt(n, i - 1));
 }
