6a3c69968e3d11cd98cae3a8125805746d6f6f4a
MILKO28 <beyumilki@gmail.com>
2023-04-05 12:13:54 -0700

    corrected

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -25,6 +25,7 @@ typedef struct listadd_s
 	struct listadd_s *next;
 } listadd_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
